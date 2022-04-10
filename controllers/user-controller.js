84abeff45a91675da7fd33c7abca08edf9439c8e
ajmarrocco <ajmarrocco@gmail.com>
2022-04-10 12:52:07 -0400

    update friends routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,14 +6,14 @@ const userController = {
     // GET /api/users
     getAllUser(req, res) {
         User.find({})
-            // .populate({
-            //     path: 'comments',
-            //     // the minus sign in front of __v indicates we want to return everything except the __v
-            //     select: '-__v'
-            // })
-            // .select('-__v')
-            // // sorts in descending order
-            // .sort({ _id: -1 })
+            .populate({
+                path: 'friends',
+                // the minus sign in front of __v indicates we want to return everything except the __v
+                select: '-__v'
+            })
+            .select('-__v')
+            // sorts in descending order
+            .sort({ _id: -1 })
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
@@ -25,11 +25,11 @@ const userController = {
     // destructured params from req
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-            // .populate({
-            //     path: 'comments',
-            //     select: '-__v'
-            // })
-            // .select('-__v')
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbUserData => {
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No user found with this id!' });
@@ -82,12 +82,33 @@ const userController = {
     },
     addFriend({ body }, res) {
         User.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    // sets id to the user ID of choice
+                    { _id: params.userId },
+                    // pushes comment to array
+                    { $push: { friends: _id } },
+                    // will return collection not updated if set to false
+                    // when set to true it will return user with updated comment
+                    { new: true, runValidators: true }
+                )
+            })
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err));
     },
     removeFriend({ params }, res) {
         // Updates and returns as a response through the find one
         User.findOneAndDelete({ _id: params.id })
+            .then(deletedComment => {
+                if (!deletedComment) {
+                    return res.status(404).json({ message: 'No comment with this id!' });
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { friends: params.userId } },
+                    { new: true }
+                );
+            })
             .then(dbUserData => {
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No user found with this id!' });
